6802769b6482770fc23f6d208893a8a09df7f5b3
Ervin Bosenbacher <admin@tradingpits.io>
2022-07-07 18:10:40 +0100

    lambda nodejs with ts support added

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,9 +1,38 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { join } from 'path'
+import { GenericTable } from './GenericTable';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
 
 export class SpaceStack extends Stack {
 
+    private api = new RestApi(this, 'SpaceAPI');
+    private spacesTable = new GenericTable(
+        'spacesTable',
+        'spaceId',
+        this
+    )
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const helloLambda = new LambdaFunction(this, 'helloLambda', {
+            runtime: Runtime.NODEJS_16_X,
+            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+            handler: 'hello.main'
+        });
+
+        const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+            entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+            handler: 'handler'
+        });
+
+        // Hello API Lambda integration
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaResource = this.api.root.addResource('hello');
+        helloLambdaResource.addMethod('GET', helloLambdaIntegration);
     }
 }
